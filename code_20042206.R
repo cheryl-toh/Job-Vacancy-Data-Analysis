e98bb1d54bebef3bdcb68f47302e3a7c19418392
cheryl-toh <cheryltqr2907@gmail.com>
2023-11-11 16:27:33 +0800

    first commit

---
+++ code_20042206.R
@@ -0,0 +1,56 @@
+# import statements
+...
+
+# scrapping data
+## job title (Gladys)
+
+## location (Gladys)
+
+## company name (Gabriel)
+
+## date posted (Gabriel)
+
+## date due (Gabriel)
+
+## salary (Cheryl)
+
+## company size (Bryan)
+
+## contract type (Bryan)
+
+## working hour (Marcus)
+
+## experience level (Marcus)
+
+## education level (Cheryl)
+
+
+# Forming Dataframe
+...
+
+
+# Data Analysis
+## Company Distribution (Gladys)
+
+## Time Trend (Gabriel)
+
+## Company size by frequency (Bryan)
+
+## Company size vs job post available duration (Gabriel)
+
+## Experience level by frequency (Bryan)
+
+## Education level by frequency (Bryan)
+
+## Contract type vs average working hour (Marcus)
+
+## Contract type by frequency (Cheryl)
+
+## Salary by job category (Cheryl)
+
+## Salary vs Experience level (Marcus)
+
+## Job distribution by location (Gladys)
+
+
+
