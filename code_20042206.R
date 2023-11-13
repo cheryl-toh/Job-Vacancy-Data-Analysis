fc0a9946472e17b34305b55904242d734db486e1
cheryl-toh <cheryltqr2907@gmail.com>
2023-11-13 18:17:05 +0800

    Changed code structure, completed salary and edu level scrape

--- code_20042206.R
+++ code_20042206.R
@@ -1,56 +1,195 @@
 # import statements
 library('rvest')
+library(stringr)
 
 # scrapping data
 
-url <- 'https://www.jobstreet.com.my/jobs/in-Malaysia'
-
-# Function to scrape data from a page
-scrape_page <- function(url) {
+## job title (Gladys)
+scrape_title <- function(url) {
   
-  ## read page url
+  # Read page URL
   page <- read_html(url)
   
-  ## job title (Gladys)
+}
 
-  ## location (Gladys)
+
+## location (Gladys)
+scrape_location <- function(url) {
   
-  ## company name (Gabriel)
-  
-  ## date posted (Gabriel)
-  
-  ## date due (Gabriel)
-  
-  ## salary (Cheryl)
-  selected_salary_elements <- html_nodes(page, ".y44q7ih+ .y44q7ih")
-  extracted_salary <- html_text(selected_salary_elements)
-  print(extracted_salary)
-  
-  ## company size (Bryan)
-  
-  ## contract type (Bryan)
-  
-  ## working hour (Marcus)
-  
-  ## experience level (Marcus)
-  
-  ## education level (Cheryl)
-  #selected_edu_elements <- page %>% html_node("#jobList article h1 a")
-  #extracted_link <- selected_edu_elements %>% html_attr("href")
-  #print(extracted_link)
+  # Read page URL
+  page <- read_html(url)
   
 }
 
 
+## company name (Gabriel)
+scrape_co_name <- function(url) {
+  
+  # Read page URL
+  page <- read_html(url)
+  
+}
 
 
-## main loop to scrap 4 pages of data
+## date posted (Gabriel)
+scrape_date <- function(url) {
+  
+  # Read page URL
+  page <- read_html(url)
+  
+}
+
+
+## salary (Cheryl)
+scrape_salary <- function(url) {
+  
+  # Read page URL
+  page <- read_html(url)
+  
+  # Select all job elements
+  job_elements <- html_nodes(page, ".rqoqz0")
+  
+  # Initialize vectors to store job titles and extracted salaries
+  extracted_salary <- NULL
+  
+  # Loop through each job element
+  for (i in 1:length(job_elements)) {
+   
+    # Check if salary information is present
+    if (length(html_text(html_nodes(job_elements[i], '.y44q7ih+ .y44q7ih'))) == 0){
+      
+      extracted_salary[i] <- NA
+      
+    }else {
+      
+      extracted_salary[i] <- html_text(html_nodes(job_elements[i],'.y44q7ih+ .y44q7ih'))
+      
+      if (grepl("monthly", extracted_salary[i], ignore.case = TRUE)) {
+        
+        # Extract numeric values from the salary text
+        salary_values <- as.numeric(gsub("[^0-9]+", "", extracted_salary[i]))
+        
+        # Calculate the average for salary ranges
+        if (length(salary_values) == 2) {
+          
+          average_salary <- mean(salary_values)
+          extracted_salary[i] <- paste(average_salary, "monthly")
+          
+        } else {
+          
+          # If there is only one value, use it as the average
+          extracted_salary[i] <- paste(salary_values, "monthly")
+          
+        }
+      }
+    }
+  }
+  
+  # Return the extracted salaries
+  return(extracted_salary)
+  
+}
+  
+
+## company size (Bryan)
+scrape_co_size <- function(url) {
+  
+  # Read page URL
+  page <- read_html(url)
+  
+}
+
+
+## job type (Bryan)
+scrape_job_type <- function(url) {
+  
+  # Read page URL
+  page <- read_html(url)
+  
+}
+
+
+## working hour (Marcus)
+scrape_working_hour <- function(url) {
+  
+  # Read page URL
+  page <- read_html(url)
+  
+}
+
+
+## experience level (Marcus)
+scrape_exp_level <- function(url) {
+  
+  # Read page URL
+  page <- read_html(url)
+
+}
+
+
+## education level (Cheryl)
+scrape_edu_level <- function(url) {
+  
+  # Read page URL
+  page <- read_html(url)
+  
+  # Define empty list for all education levels
+  all_education_levels <- list()
+  
+  # Select all job links
+  job_links <- page %>% html_nodes("#jobList article h1 a") %>% html_attr("href")
+  
+  # Loop through each job link
+  for (job_link in job_links) {
+    
+    # Construct the full URL for the job article
+    article_url <- paste0("https://www.jobstreet.com.my", job_link)  # Replace with the actual base URL
+    
+    # Add delay to prevent request limit error
+    Sys.sleep(1)
+    
+    # Navigate to the article page
+    article_page <- read_html(article_url)
+    
+    # Replace ".your-class" with the actual class containing education information
+    education_element <- article_page %>% html_node(".pmwfa57:nth-child(2) .y44q7i1")
+    
+    # Extract text from the education element
+    extracted_education <- html_text(education_element)
+    
+    # Split the extracted education text into a list of education levels
+    edu_levels <- unlist(strsplit(extracted_education, ", "))
+    
+    # Append the list of education levels to the parent list
+    all_education_levels <- c(all_education_levels, list(edu_levels))
+    
+    # Explicitly close the connection
+    rm(article_page)
+  }
+  
+  # Return the extracted education levels
+  return(all_education_levels)
+  
+}
+
+
+## Main loop to scrape 4 pages of data
+all_salaries <- NULL
+all_education_levels <- NULL
+url <- 'https://www.jobstreet.com.my/jobs/in-Malaysia'
+
+print("Scrapping webpages... (Might take up to 3 - 5 minutes)")
+
 for (page_number in 1:4) {
-  
   page_url <- paste0(url, "?pg=", page_number)
-  page_data <- scrape_page(page_url)
-  
+  all_salaries <- c(all_salaries, scrape_salary(page_url))
+  all_education_levels <- c(all_education_levels, scrape_edu_level(page_url))
 }
+
+# Print the results
+print(length(all_salaries))
+print(length(all_education_levels))
+
 
 # Forming Data frame
 
